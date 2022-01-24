c0adbd551a692fe6f3e74886fdc1f716e8566383
urgent <urgent.programmer@gmail.com>
2022-01-24 16:35:41 -0500

    #2 -- :recycle: Fix spacing, indents, remove commented code

--- src/pages/GenSummary.js
+++ src/pages/GenSummary.js
@@ -9,34 +9,34 @@ import * as React from 'react';
 
 
 
-export default function GenSummary(){
-    return (
-        <div>
-         <Typography variant="h4" color='charcoal' fontWeight= '500'>
-          Generators
-          </Typography>
+export default function GenSummary() {
+  return (
+    <div>
+      <Typography variant="h4" color='charcoal' fontWeight='500'>
+        Generators
+      </Typography>
 
-        <Box sx={{ width: '100%',backgroundColor:'lightgray' }}>
-            <Grid container spacing={1} style={{ paddingLeft:'15px',paddingBottom:'8px' }}>
-  
-                <Grid item xs={3}>
-                <UnitSelectorDynamic /> 
-                </Grid>
+      <Box sx={{ width: '100%', backgroundColor: 'lightgray' }}>
+        <Grid container spacing={1} style={{ paddingLeft: '15px', paddingBottom: '8px' }}>
 
-
-            <Grid item xs={2} style={{ paddingLeft:'8px',paddingTop:'12px' }}> 
-                    <ReactDatePicker /> 
-                </Grid>
-            
-            </Grid>
-        </Box>
-     
-          <Grid container spacing={1} sx={{ paddingLeft:'15px' }}>
-         
-            <MyTabs />
-           
+          <Grid item xs={3}>
+            <UnitSelectorDynamic />
           </Grid>
-        
-        </div>
-      );
-    }
+
+
+          <Grid item xs={2} style={{ paddingLeft: '8px', paddingTop: '12px' }}>
+            <ReactDatePicker />
+          </Grid>
+
+        </Grid>
+      </Box>
+
+      <Grid container spacing={1} sx={{ paddingLeft: '15px' }}>
+
+        <MyTabs />
+
+      </Grid>
+
+    </div>
+  );
+}
